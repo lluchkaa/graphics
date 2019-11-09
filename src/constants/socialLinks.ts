ebe5d9a495e83a1747ede938b0ef4b73ffbb8e48
lluchkaa <lluchkaa@gmail.com>
2019-11-09 15:19:08 +0200

    links added

---
+++ src/constants/socialLinks.ts
@@ -0,0 +1,18 @@
+import ISocialLink, { SocialLinkType } from '../interfaces/ISocialLink'
+
+const links: ISocialLink[] = [
+  {
+    key: SocialLinkType.facebook,
+    href: '#'
+  },
+  {
+    key: SocialLinkType.twitter,
+    href: '#'
+  },
+  {
+    key: SocialLinkType.instagram,
+    href: '#'
+  }
+]
+
+export default links
