ccc350f5348e5ffe6c4e785e03eed8366a7a8590
lluchkaa <lluchkaa@gmail.com>
2019-11-09 19:58:24 +0200

    complex created with base operators

---
+++ src/services/Complex/index.ts
@@ -0,0 +1,53 @@
+class Complex {
+  private _real: number
+  private _imag: number
+
+  public constructor(real: number = 0, imag: number = 0) {
+    this._real = real
+    this._imag = imag
+  }
+
+  public set real(v: number) {
+    this._real = v;
+  }
+
+  public get real(): number {
+    return this._real
+  }
+
+  public set imag(v: number) {
+    this._imag = v;
+  }
+
+  public get imag(): number {
+    return this._imag
+  }
+
+  public static add = (nums: Complex[]): Complex =>
+    nums.reduce((acc, v) => new Complex(acc.real + v.real, acc.imag + acc.imag))
+
+  public static sub = (nums: Complex[]): Complex =>
+    nums.reduce((acc, v) => new Complex(acc.real - v.real, acc.imag - acc.imag))
+
+  public static mul = (nums: Complex[]): Complex =>
+    nums.reduce((acc, v) =>
+      new Complex(
+        acc.real * v.real - acc.imag * v.imag,
+        acc.real * v.imag + acc.imag * v.real
+      )
+    )
+
+  public static div = (nums: Complex[]): Complex =>
+    nums.reduce((acc, v) => {
+      const denom = (
+        v.real * v.real + v.imag * v.imag
+      )
+      return new Complex(
+        (acc.real * v.real + acc.imag * v.imag) / denom,
+        (acc.imag * v.real - acc.real * v.imag) / denom
+      )
+    })
+
+}
+
+export default Complex
