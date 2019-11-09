7fac05aaea99373c56ca5c3e4398d82068181233
lluchkaa <lluchkaa@gmail.com>
2019-11-09 15:03:20 +0200

    Landing page added

---
+++ src/components/pages/Landing/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface IProps { }
+interface IState { }
+
+class Landing extends React.Component<IProps, IState> {
+  render() {
+    return (
+      <div />
+    )
+  }
+}
+
+export default Landing
