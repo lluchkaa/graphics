1ac5b2695d8bde16c3bca1193adcfab7327654ba
lluchkaa <lluchkaa@gmail.com>
2019-11-09 15:55:20 +0200

    getContentImage added

--- src/constants/contentLinks.ts
+++ src/constants/contentLinks.ts
@@ -1,6 +1,9 @@
 import IContentLink, { ContentLinkType } from '../interfaces/IContentLink'
-
 import routeLinks from '../routes/links'
+
+import fractalImage from '../assets/images/contentLogos/fractal.png'
+import colorSchemeImage from '../assets/images/contentLogos/colorScheme.png'
+import hexagonImage from '../assets/images/contentLogos/hexagon.svg'
 
 const links: IContentLink[] = [
   {
@@ -16,5 +19,18 @@ const links: IContentLink[] = [
     href: routeLinks.hexagon()
   }
 ]
+
+export const getContentImage = (key: ContentLinkType) => {
+  switch (key) {
+    case ContentLinkType.fractal:
+      return fractalImage
+    case ContentLinkType.colorScheme:
+      return colorSchemeImage
+    case ContentLinkType.hexagon:
+      return hexagonImage
+    default:
+      return ''
+  }
+}
 
 export default links
