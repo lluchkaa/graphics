1803c07bf66a71ed5150c3d13a07276ff54537d0
lluchkaa <lluchkaa@gmail.com>
2019-11-09 15:28:18 +0200

    getting social link image

--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -1,5 +1,9 @@
 import ISocialLink, { SocialLinkType } from '../interfaces/ISocialLink'
 
+import facebookImage from '../assets/images/social/facebook.svg'
+import twitterImage from '../assets/images/social/twitter.svg'
+import instagramImage from '../assets/images/social/instagram.svg'
+
 const links: ISocialLink[] = [
   {
     key: SocialLinkType.facebook,
@@ -14,5 +18,18 @@ const links: ISocialLink[] = [
     href: '#'
   }
 ]
+
+export const getSocialImage = (key: SocialLinkType) => {
+  switch (key) {
+    case SocialLinkType.facebook:
+      return facebookImage
+    case SocialLinkType.twitter:
+      return twitterImage
+    case SocialLinkType.instagram:
+      return instagramImage
+    default:
+      return ''
+  }
+}
 
 export default links
