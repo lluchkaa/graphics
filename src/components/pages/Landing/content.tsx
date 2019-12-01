8ce52c1ef9bd60f1feb08be735290730d857e5f5
AnastasiaTarasenko123 <anastasiyatarasenko2000@gmail.com>
2019-12-01 18:17:49 +0200

    social-link

--- src/components/pages/Landing/content.tsx
+++ src/components/pages/Landing/content.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { Link } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
+import Link from '@material-ui/core/Link'
 
 import path from '../../../routes/paths'
 
@@ -12,35 +13,51 @@ const logo = require("../../../assets/images/logo.png");
 const hexagon = require("../../../assets/images/contentLogos/hexagon.png");
 const colorScheme = require("../../../assets/images/contentLogos/colorScheme.png");
 const fractal = require("../../../assets/images/contentLogos/fractal.png");
+const facebook = require("../../../assets/images/social/facebook.png");
+const instagram = require("../../../assets/images/social/instagram.png");
+const twitter = require("../../../assets/images/social/twitter.png");
 
 const Content: React.FC<IProps> = (props: IProps) => (
   <div
     className="page landing"
   >
-    <Link to={path.userGuide}>
+    <RouterLink to={path.userGuide}>
       <p className="user-guide-text">?</p>
-    </Link>
+    </RouterLink>
     <img src={logo} alt="logo" />
     <div className="main-navigation">
-      <Link to={path.hexagon}>
+      <RouterLink to={path.hexagon}>
         <div className="card hexagon-link">
           <img src={hexagon} alt="hexagon" className="img-logo" />
           <p className="title-link">Hexagon</p>
         </div>
-      </Link>
-      <Link to={path.colorScheme}>
-      <div className="card color-scheme-link">
-        <img src={colorScheme} alt="color scheme" className="img-logo" />
-        <p className="title-link">Color Scheme</p>
-      </div>
-      </Link>
-      <Link to={path.fractal}>
-      <div className="card fractal-link">
-        <img src={fractal} alt="fractal" className="img-logo" />
-        <p className="title-link">Fractal</p>
-      </div>
-      </Link>
+      </RouterLink>
+      <RouterLink to={path.colorScheme}>
+        <div className="card color-scheme-link">
+          <img src={colorScheme} alt="color scheme" className="img-logo" />
+          <p className="title-link">Color Scheme</p>
+        </div>
+      </RouterLink>
+      <RouterLink to={path.fractal}>
+        <div className="card fractal-link">
+          <img src={fractal} alt="fractal" className="img-logo" />
+          <p className="title-link">Fractal</p>
+        </div>
+      </RouterLink>
     </div>
+    <footer className="block-social-links">
+      <div className="social-links">
+        <Link href="https://www.facebook.com" target="_blank">
+          <img src={facebook} alt="facebook" className="img-social" />
+        </Link>
+        <Link href="https://www.instagram.com" target="_blank">
+          <img src={instagram} alt="instagram" className="img-social" />
+        </Link>
+        <Link href="https://twitter.com" target="_blank">
+          <img src={twitter} alt="twitter" className="img-social" />
+        </Link>
+      </div>
+    </footer>
   </div >
 )
 
