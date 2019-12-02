d735a2f6be314edc2f0e0afdf7f4dca6b4085d13
lluchkaa <lluchkaa@gmail.com>
2019-12-03 01:38:23 +0200

    constructors added

--- src/services/Vector/Vector2d.ts
+++ src/services/Vector/Vector2d.ts
@@ -1,3 +1,5 @@
+import { IPoint2d } from '../../interfaces/IPoint'
+
 class Vector2d {
   private _x: number
   private _y: number
@@ -9,6 +11,8 @@ class Vector2d {
 
   public static fromAbsAng = (abs: number, ang: number) =>
     new Vector2d(abs * Math.cos(ang), abs * Math.sin(ang))
+    
+  public static fromPoint = (p: IPoint2d) => new Vector2d(p.x, p.y)
 
   public get x(): number {
     return this._x
@@ -32,6 +36,8 @@ class Vector2d {
   mul = (v: Vector2d) => this._x * v._x + this._y * v._y
   abs = () => Math.sqrt(this._x * this._x + this._y * this._y)
   ang = () => Math.atan(this._y / this._x)
+
+  toPoint = (): IPoint2d => ({ x: this._x, y: this._y })
 }
 
 export default Vector2d
